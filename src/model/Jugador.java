d47b30704c0e3660d86f12502e6571c9569e68ef
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-28 12:09:36 -0500

    commit - Jugador .

--- src/model/Jugador.java
+++ src/model/Jugador.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Jugador {
-}
+    private int id;
+    private String nombre;
+
+    public Jugador(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+}   
