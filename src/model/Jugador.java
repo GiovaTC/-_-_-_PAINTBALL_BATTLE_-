e2eeab1342725d5c3a4caf9ffa9189583f0e963b
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-28 12:14:15 -0500

    commit - Partida / .

--- src/model/Jugador.java
+++ src/model/Jugador.java
@@ -11,4 +11,4 @@ public class Jugador {
 
     public int getId() { return id; }
     public String getNombre() { return nombre; }
-}   
+}
--- src/model/Partida.java
+++ src/model/Partida.java
@@ -1,4 +1,31 @@
 package model;
 
 public class Partida {
+    private int jugadorId;
+    private int puntaje;
+    private int disparos;
+    private long duracionSeg;
+
+    public Partida(int jugadorId, int puntaje, int disparos, long duracionSeg) {
+        this.jugadorId = jugadorId;
+        this.puntaje = puntaje;
+        this.disparos = disparos;
+        this.duracionSeg = duracionSeg;
+    }
+
+    public int getJugadorId() {
+        return jugadorId;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public int getDisparos() {
+        return disparos;
+    }
+
+    public long getDuracionSeg() {
+        return duracionSeg;
+    }
 }
